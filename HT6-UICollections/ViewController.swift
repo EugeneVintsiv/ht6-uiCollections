fafb5e79d1c131649ec62341b39b536058bd01be
Eugene <eugene.vintsiv@gmail.com>
2018-12-06 02:16:25 +0200

    1) add table view to storyboard 2) code for manage view in ViewController

--- HT6-UICollections/ViewController.swift
+++ HT6-UICollections/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NotesViewController: UIViewController {
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    var elements = [CellData]()
+    
     override func viewDidLoad() {
+        addButton.isHidden = true
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        elements = [
+            CellData.init(title: "Title1", description: "Description1", creationDate: "2018-01-01"),
+            CellData.init(title: "Title2", description: "Description2", creationDate: "2018-01-02"),
+            CellData.init(title: "Title3", description: "Description3", creationDate: "2018-01-03"),
+            CellData.init(title: "Title4", description: "Description4", creationDate: "2018-01-04")
+        ]
+        
+        print(elements.count)
+        let cellNib = UINib(nibName: "NoteViewCell", bundle: nil)
+        print(cellNib)
+        tableView.register(cellNib, forCellReuseIdentifier: NoteViewCell.reuseIdentifier)
+        tableView.reloadData()
     }
+}
 
-
+// MARK: - UITableViewDataSource
+extension NotesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NoteViewCell.reuseIdentifier, for: indexPath) as! NoteViewCell
+        
+        cell.backgroundColor = UIColor.green
+        cell.noteTitle.text = elements[indexPath.row].title
+        cell.noteDate.text = elements[indexPath.row].creationDate
+        cell.noteDescription.text = elements[indexPath.row].description
+        
+        return cell
+    }
 }
 
